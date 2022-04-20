00a0e74eaaf8059878f21fcccbee29a448ee6849
Jarrod <jarrodgro@gmail.com>
2022-04-19 19:16:03 -0500

    progress on battle window

--- game.js
+++ game.js
@@ -2059,6 +2059,20 @@ class Game{
     _handlePlayerMoves()
     {
         let move_list = this["_queuedMoves_" + this._currentPlayerTurn];
+
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        
         for (let i = 0; i < move_list.length; i++)
         {
             // Ensure that the move is still valid
