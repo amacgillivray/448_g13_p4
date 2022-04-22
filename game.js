b16ea60c08c5b239d45f0462b14ce1f76f7e450e
mccumberluke <mccumberluke@gmail.com>
2022-04-21 21:02:50 -0500

    drag function

--- game.js
+++ game.js
@@ -1791,6 +1791,7 @@ class Battle {
 
 }
 
+//Not neccessarily neeed 
 class Strike {
 
     constructor(strikeForce, target)
@@ -2395,3 +2396,40 @@ let log_entries = 0;
 let battle_ct = 0;
 let turn_ct = 0;
 let game = new Game;
+
+dragElemnet(document.getElementById("mydiv"));
+function dragElemnet(element){
+    var pos1 = 0, pos2= 0, pos3 = 0, pos4 = 0,
+    if(document.getElementById(element.id + "header")){
+        document.getElementById(element.id + "header").onmousedown = dragMouseDown;
+    } 
+    else{
+        element.onmousedown = dragMouseDown;
+    }
+    function dragMouseDown(e){
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        //function call for cursor move
+        document.onmousemove = elementDrag; 
+    }
+    function elementDrag(e){
+        e = e || window.event;
+        e.preventDefault();
+        //new cursor position
+        pos1 = pos3-e.clientX;
+        pos2 = pos4-e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        //element new position
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos 1) + "px";
+    }
+    function closeDragElement(){
+        //mouse is realease when movement stopped
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
