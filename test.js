fe82a395ad1ecdd6a9a65106f193a8cdbf24ca1c
mccumberluke <mccumberluke@gmail.com>
2022-04-22 16:54:20 -0500

    added test.js to html and file

---
+++ test.js
@@ -0,0 +1,17 @@
+function test(){
+
+}
+
+//Tests
+//Gamelog
+//get best troop count symbol => 300 outputs batallion
+//Iscapital region 
+//GameUI => set region owner (test for of, bf, neutral)
+//Get units in region 
+//draw movement arrow  
+//force: alter force, distribute damage side
+//Unit: update health alter units
+//Battle: start 
+//Game: change turn, handle win, handle playermoves 
+
+
