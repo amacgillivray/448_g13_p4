a2235257c97d9edd874521d8388cc4f7dadb6442
Andrew MacGillivray <agjmacgill@gmail.com>
2022-04-21 21:01:40 -0500

    BattleWindow changes + misc ... + Add GameMap.drawBattleWindow() fn (part of #9) + Battle class now tracks region terrain; chooses 3 random terrain types (part of #9, #6) + Adds terrain_types array to help crawl region_terrain in loops + Adds craters to all terrain details as prep for #27
    - Remove old battle window code from game._handlePlayerMoves()

--- game.js
+++ game.js
@@ -248,107 +248,115 @@ const region_connections = {
     j9: ["j5", "j6", "j8", "i4", "i9", "i8"]
 };
 
+const terrain_types = [
+    "craters",
+    "plains",
+    "forest",
+    "water",
+    "urban"
+];
+
 const region_terrain = {
-    a0: {plains: 0.9, forest: 0, water: 0, urban:0 },
-    a1: {plains: 0.6, forest: 0, water: 0, urban:0 },
-    a2: {plains: 0, forest: 0.5, water: 0, urban:0 },
-    a3: {plains: 0, forest: 0.3, water: 0, urban:0 },
-    a4: {plains: 0, forest: 0.5, water: 0, urban:0 },
-    a5: {plains: 0, forest: 0.5, water: 0, urban:0 },
-    a6: {plains: 0, forest: 0.3, water: 0, urban:0 },
-    a7: {plains: 0, forest: 0.5, water: 0.2, urban:0.5 },
-    a8: {plains: 0.4, forest: 0.3, water: 0, urban:0 },
-    a9: {plains: 0.5, forest: 0.4, water: 0, urban:0 },
-    b0: {plains: 0.9, forest: 0.1, water: 0, urban:0 },
-    b1: {plains: 0.5, forest: 0.1, water: 0, urban:0 },
-    b2: {plains: 0.7, forest: 0.1, water: 0.1, urban:0 },
-    b3: {plains: 0.8, forest: 0, water: 0, urban:0 },
-    b4: {plains: 0, forest: 0, water: 0.1, urban:0 },
-    b5: {plains: 0, forest: 0, water: 0.3, urban:0.6 },
-    b6: {plains: 0, forest: 0, water: 0.3, urban:0.5 },
-    b7: {plains: 0, forest: 0, water: 0.2, urban:0.8 },
-    b8: {plains: 0, forest: 0, water: 0.2, urban:0.5 },
-    b9: {plains: 0, forest: 0, water: 0.3, urban:0 },
-    c0: {plains: 0, forest: 0, water: 0.4, urban:0 },
-    c1: {plains: 0, forest: 0, water: 0.4, urban:0.5 },
-    c2: {plains: 0, forest: 0, water: 0.2, urban:0.8 },
-    c3: {plains: 0, forest: 0, water: 0.1, urban:0.9 },
-    c4: {plains: 0, forest: 0, water: 0.1, urban:0.8 },
-    c5: {plains: 0, forest: 0, water: 0.2, urban:0 },
-    c6: {plains: 0, forest: 0.2, water: 0.2, urban:0 },
-    c7: {plains: 0.3, forest: 0, water: 0, urban:0 },
-    c8: {plains: 0, forest: 0, water: 0, urban:0.1 },
-    c9: {plains: 0, forest: 0, water: 0.2, urban:0.2 },
-    d0: {plains: 0, forest: 0, water: 0.2, urban:0 },
-    d1: {plains: 0, forest: 0.3, water: 0, urban:0.2 },
-    d2: {plains: 0, forest: 0.2, water: 0.2, urban:0.6 },
-    d3: {plains: 0, forest: 0.2, water: 0.1, urban:0.5 },
-    d4: {plains: 0, forest: 0, water: 0.1, urban:0 },
-    d5: {plains: 0, forest: 0, water: 0, urban:0 },
-    d6: {plains: 0.5, forest: 0, water: 0, urban:0 },
-    d7: {plains: 0.8, forest: 0, water: 0, urban:0 },
-    d8: {plains: 0, forest: 0, water: 0, urban:0 },
-    d9: {plains: 0, forest: 0, water: 0, urban:0 },
-    e0: {plains: 0.5, forest: 0, water: 0.2, urban:0 },
-    e1: {plains: 0, forest: 0.5, water: 0.2, urban:0 },
-    e2: {plains: 0.5, forest: 0.3, water: 0, urban:0 },
-    e3: {plains: 0, forest: 0.2, water: 0, urban:0.5 },
-    e4: {plains: 0, forest: 0.2, water: 0.2, urban:0.5 },
-    e5: {plains: 0, forest: 0.2, water: 0.2, urban:0.5 },
-    e6: {plains: 0, forest: 0.9, water: 0, urban:0 },
-    e7: {plains: 0, forest: 0, water: 0, urban:0 },
-    e8: {plains: 0, forest: 0, water: 0, urban:0 },
-    e9: {plains: 0, forest: 0, water: 0, urban:0 },
-    f0: {plains: 1, forest: 0, water: 0, urban:0 },
-    f1: {plains: 0.7, forest: 0, water: 0.1, urban:0 },
-    f2: {plains: 0, forest: 0, water: 0.2, urban:0 },
-    f3: {plains: 0, forest: 0, water: 0.2, urban:0.4 },
-    f4: {plains: 0, forest: 0, water: 0.2, urban:0.8 },
-    f5: {plains: 0.3, forest: 0, water: 0.2, urban:0.3 },
-    f6: {plains: 0.3, forest: 0, water: 0, urban:0 },
-    f7: {plains: 0, forest: 0.3, water: 0.3, urban:0.4 },
-    f8: {plains: 0, forest: 0.3, water: 0, urban:0 },
-    f9: {plains: 0, forest: 0.6, water: 0.1, urban:0.3 },
-    g0: {plains: 0, forest: 0.5, water: 0, urban:0 },
-    g1: {plains: 0, forest: 0.3, water: 0, urban:0 },
-    g2: {plains: 0, forest: 0.3, water: 0, urban:0 },
-    g3: {plains: 0.7, forest: 0, water: 0, urban:0 },
-    g4: {plains: 0.1, forest: 0, water: 0.1, urban:0 },
-    g5: {plains: 0.2, forest: 0.1, water: 0, urban:0 },
-    g6: {plains: 0, forest: 0.4, water: 0, urban:0 },
-    g7: {plains: 0, forest: 1, water: 0, urban:0 },
-    g8: {plains: 0.4, forest: 0.3, water: 0, urban:0 },
-    g9: {plains: 0.7, forest: 0, water: 0.2, urban:0 },
-    h0: {plains: 0, forest: 0, water: 0, urban:0 },
-    h1: {plains: 0, forest: 0.3, water: 0, urban:0 },
-    h2: {plains: 0.3, forest: 0, water: 0, urban:0 },
-    h3: {plains: 0.5, forest: 0.1, water: 0, urban:0 },
-    h4: {plains: 0, forest: 0.6, water: 0, urban:0 },
-    h5: {plains: 0.2, forest: 0.5, water: 0, urban:0 },
-    h6: {plains: 0.8, forest: 0, water: 0, urban:0 },
-    h7: {plains: 0, forest: 1, water: 0, urban:0 },
-    h8: {plains: 0, forest: 0.4, water: 0, urban:0 },
-    h9: {plains: 0.5, forest: 0, water: 0, urban:0 },
-    i0: {plains: 0.5, forest: 0.2, water: 0, urban:0 },
-    i1: {plains: 0.5, forest: 0, water: 0.1, urban:0 },
-    i2: {plains: 0, forest: 0, water: 0.2, urban:0 },
-    i3: {plains: 0.3, forest: 0.4, water: 0.1, urban:0 },
-    i4: {plains: 0, forest: 0.9, water: 0, urban:0 },
-    i5: {plains: 0.5, forest: 0.2, water: 0, urban:0 },
-    i6: {plains: 0.2, forest: 0.4, water: 0, urban:0 },
-    i7: {plains: 0.3, forest: 0.5, water: 0, urban:0 },
-    i8: {plains: 0.3, forest: 0.3, water: 0, urban:0 },
-    i9: {plains: 0.4, forest: 0.5, water: 0, urban:0 },
-    j0: {plains: 0, forest: 0.2, water: 0.2, urban:0.6 },
-    j1: {plains: 0, forest: 0.5, water: 0, urban:0.2 },
-    j2: {plains: 0, forest: 0.4, water: 0, urban:0 },
-    j3: {plains: 0, forest: 0.4, water: 0, urban:0 },
-    j4: {plains: 0, forest: 0.3, water: 0, urban:0 },
-    j5: {plains: 0, forest: 0.7, water: 0, urban:0 },
-    j6: {plains: 0, forest: 0.8, water: 0, urban:0 },
-    j7: {plains: 0, forest: 0.6, water: 0, urban:0 },
-    j8: {plains: 0.3, forest: 0.7, water: 0, urban:0 },
-    j9: {plains: 0.2, forest: 0.8, water: 0, urban:0 },
+    a0: {craters: 0, plains: 0.9, forest: 0, water: 0, urban:0 },
+    a1: {craters: 0, plains: 0.6, forest: 0, water: 0, urban:0 },
+    a2: {craters: 0, plains: 0, forest: 0.5, water: 0, urban:0 },
+    a3: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0 },
+    a4: {craters: 0, plains: 0, forest: 0.5, water: 0, urban:0 },
+    a5: {craters: 0, plains: 0, forest: 0.5, water: 0, urban:0 },
+    a6: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0 },
+    a7: {craters: 0, plains: 0, forest: 0.5, water: 0.2, urban:0.5 },
+    a8: {craters: 0, plains: 0.4, forest: 0.3, water: 0, urban:0 },
+    a9: {craters: 0, plains: 0.5, forest: 0.4, water: 0, urban:0 },
+    b0: {craters: 0, plains: 0.9, forest: 0.1, water: 0, urban:0 },
+    b1: {craters: 0, plains: 0.5, forest: 0.1, water: 0, urban:0 },
+    b2: {craters: 0, plains: 0.7, forest: 0.1, water: 0.1, urban:0 },
+    b3: {craters: 0, plains: 0.8, forest: 0, water: 0, urban:0 },
+    b4: {craters: 0, plains: 0, forest: 0, water: 0.1, urban:0 },
+    b5: {craters: 0, plains: 0, forest: 0, water: 0.3, urban:0.6 },
+    b6: {craters: 0, plains: 0, forest: 0, water: 0.3, urban:0.5 },
+    b7: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0.8 },
+    b8: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0.5 },
+    b9: {craters: 0, plains: 0, forest: 0, water: 0.3, urban:0 },
+    c0: {craters: 0, plains: 0, forest: 0, water: 0.4, urban:0 },
+    c1: {craters: 0, plains: 0, forest: 0, water: 0.4, urban:0.5 },
+    c2: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0.8 },
+    c3: {craters: 0, plains: 0, forest: 0, water: 0.1, urban:0.9 },
+    c4: {craters: 0, plains: 0, forest: 0, water: 0.1, urban:0.8 },
+    c5: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0 },
+    c6: {craters: 0, plains: 0, forest: 0.2, water: 0.2, urban:0 },
+    c7: {craters: 0, plains: 0.3, forest: 0, water: 0, urban:0 },
+    c8: {craters: 0, plains: 0, forest: 0, water: 0, urban:0.1 },
+    c9: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0.2 },
+    d0: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0 },
+    d1: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0.2 },
+    d2: {craters: 0, plains: 0, forest: 0.2, water: 0.2, urban:0.6 },
+    d3: {craters: 0, plains: 0, forest: 0.2, water: 0.1, urban:0.5 },
+    d4: {craters: 0, plains: 0, forest: 0, water: 0.1, urban:0 },
+    d5: {craters: 0, plains: 0, forest: 0, water: 0, urban:0 },
+    d6: {craters: 0, plains: 0.5, forest: 0, water: 0, urban:0 },
+    d7: {craters: 0, plains: 0.8, forest: 0, water: 0, urban:0 },
+    d8: {craters: 0, plains: 0, forest: 0, water: 0, urban:0 },
+    d9: {craters: 0, plains: 0, forest: 0, water: 0, urban:0 },
+    e0: {craters: 0, plains: 0.5, forest: 0, water: 0.2, urban:0 },
+    e1: {craters: 0, plains: 0, forest: 0.5, water: 0.2, urban:0 },
+    e2: {craters: 0, plains: 0.5, forest: 0.3, water: 0, urban:0 },
+    e3: {craters: 0, plains: 0, forest: 0.2, water: 0, urban:0.5 },
+    e4: {craters: 0, plains: 0, forest: 0.2, water: 0.2, urban:0.5 },
+    e5: {craters: 0, plains: 0, forest: 0.2, water: 0.2, urban:0.5 },
+    e6: {craters: 0, plains: 0, forest: 0.9, water: 0, urban:0 },
+    e7: {craters: 0, plains: 0, forest: 0, water: 0, urban:0 },
+    e8: {craters: 0, plains: 0, forest: 0, water: 0, urban:0 },
+    e9: {craters: 0, plains: 0, forest: 0, water: 0, urban:0 },
+    f0: {craters: 0, plains: 1, forest: 0, water: 0, urban:0 },
+    f1: {craters: 0, plains: 0.7, forest: 0, water: 0.1, urban:0 },
+    f2: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0 },
+    f3: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0.4 },
+    f4: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0.8 },
+    f5: {craters: 0, plains: 0.3, forest: 0, water: 0.2, urban:0.3 },
+    f6: {craters: 0, plains: 0.3, forest: 0, water: 0, urban:0 },
+    f7: {craters: 0, plains: 0, forest: 0.3, water: 0.3, urban:0.4 },
+    f8: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0 },
+    f9: {craters: 0, plains: 0, forest: 0.6, water: 0.1, urban:0.3 },
+    g0: {craters: 0, plains: 0, forest: 0.5, water: 0, urban:0 },
+    g1: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0 },
+    g2: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0 },
+    g3: {craters: 0, plains: 0.7, forest: 0, water: 0, urban:0 },
+    g4: {craters: 0, plains: 0.1, forest: 0, water: 0.1, urban:0 },
+    g5: {craters: 0, plains: 0.2, forest: 0.1, water: 0, urban:0 },
+    g6: {craters: 0, plains: 0, forest: 0.4, water: 0, urban:0 },
+    g7: {craters: 0, plains: 0, forest: 1, water: 0, urban:0 },
+    g8: {craters: 0, plains: 0.4, forest: 0.3, water: 0, urban:0 },
+    g9: {craters: 0, plains: 0.7, forest: 0, water: 0.2, urban:0 },
+    h0: {craters: 0, plains: 0, forest: 0, water: 0, urban:0 },
+    h1: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0 },
+    h2: {craters: 0, plains: 0.3, forest: 0, water: 0, urban:0 },
+    h3: {craters: 0, plains: 0.5, forest: 0.1, water: 0, urban:0 },
+    h4: {craters: 0, plains: 0, forest: 0.6, water: 0, urban:0 },
+    h5: {craters: 0, plains: 0.2, forest: 0.5, water: 0, urban:0 },
+    h6: {craters: 0, plains: 0.8, forest: 0, water: 0, urban:0 },
+    h7: {craters: 0, plains: 0, forest: 1, water: 0, urban:0 },
+    h8: {craters: 0, plains: 0, forest: 0.4, water: 0, urban:0 },
+    h9: {craters: 0, plains: 0.5, forest: 0, water: 0, urban:0 },
+    i0: {craters: 0, plains: 0.5, forest: 0.2, water: 0, urban:0 },
+    i1: {craters: 0, plains: 0.5, forest: 0, water: 0.1, urban:0 },
+    i2: {craters: 0, plains: 0, forest: 0, water: 0.2, urban:0 },
+    i3: {craters: 0, plains: 0.3, forest: 0.4, water: 0.1, urban:0 },
+    i4: {craters: 0, plains: 0, forest: 0.9, water: 0, urban:0 },
+    i5: {craters: 0, plains: 0.5, forest: 0.2, water: 0, urban:0 },
+    i6: {craters: 0, plains: 0.2, forest: 0.4, water: 0, urban:0 },
+    i7: {craters: 0, plains: 0.3, forest: 0.5, water: 0, urban:0 },
+    i8: {craters: 0, plains: 0.3, forest: 0.3, water: 0, urban:0 },
+    i9: {craters: 0, plains: 0.4, forest: 0.5, water: 0, urban:0 },
+    j0: {craters: 0, plains: 0, forest: 0.2, water: 0.2, urban:0.6 },
+    j1: {craters: 0, plains: 0, forest: 0.5, water: 0, urban:0.2 },
+    j2: {craters: 0, plains: 0, forest: 0.4, water: 0, urban:0 },
+    j3: {craters: 0, plains: 0, forest: 0.4, water: 0, urban:0 },
+    j4: {craters: 0, plains: 0, forest: 0.3, water: 0, urban:0 },
+    j5: {craters: 0, plains: 0, forest: 0.7, water: 0, urban:0 },
+    j6: {craters: 0, plains: 0, forest: 0.8, water: 0, urban:0 },
+    j7: {craters: 0, plains: 0, forest: 0.6, water: 0, urban:0 },
+    j8: {craters: 0, plains: 0.3, forest: 0.7, water: 0, urban:0 },
+    j9: {craters: 0, plains: 0.2, forest: 0.8, water: 0, urban:0 },
 };
 
 const game_setup = {
@@ -1192,6 +1200,94 @@ class GameMap {
             }
         }
     }
+
+    // 
+    static drawBattleWindow ( battle ) 
+    {
+        const drawBattleDisplay = false;
+        // Get the modal
+        let modal = document.getElementById("battleWindow");
+
+        // Get the element that closes the modal
+        let span = document.getElementById("bw_close");
+
+        let display = document.getElementById("bw_display");
+
+        let attackers = battle._off;
+        let defender  = battle._def;
+
+        let flank_titles = [
+            document.getElementById("bw_fl_t"),
+            document.getElementById("bw_fm_t"),
+            document.getElementById("bw_fr_t")
+        ];
+
+        // Set the flank titles to reflect their respective terrain type.
+        for (let i = 0; i < flank_titles.length; i++)
+        {
+            flank_titles[i].innerHTML = battle.terrain[i].toUpperCase();
+        }
+
+        // Clone the attacking and defending region elements to the display
+        if (drawBattleDisplay) {
+            let ar = document.getElementById(attackers._region);
+            let dr = document.getElementById(defender._region );
+            ["countries-inert", "regions", "plains", "forest", "urban-dense", "water", "crater-layer", "clouds"].forEach(
+                (terrain) => {
+                    let t_n = document.getElementById(terrain);
+                    let clone = t_n.cloneNode(true);
+                    clone.setAttribute("id", clone.getAttribute("id") + "_bw_display");
+                    display.innerHTML+=clone.outerHTML.replace(/id\=\"..\"/gi, "");
+                }
+            );
+            [ar, dr].forEach( (rgn) => {
+                let clone = rgn.cloneNode(true);
+                clone.setAttribute("id", clone.getAttribute("id") + "_bw_display");
+                display.innerHTML+=clone.outerHTML.replace(/id\=\"..r\"|reinforceable|cpt/gi, "");
+                rgn = document.getElementById(rgn.getAttribute("id") + "_bw_display");
+            });
+
+            // Set display box viewport to zoom in on affected regions
+            ar = ar.getBBox();
+            dr = dr.getBBox();
+            let xmin = Math.min( ar.x, dr.x ) - Math.max( ar.width, dr.width );
+            // let xmax = xmin + Math.max( ar.width, dr.width ) - Math.min( ar.width, dr.width ) - 15;
+            // let xmax = xmin + 10;
+            let xmax = xmin + 2*Math.max( ar.width, dr.width );
+
+            let ymin = Math.min( ar.y, dr.y ) - Math.max( ar.height, dr.height );
+            // let ymax = ymin + 10;
+            // let ymax = ymin + Math.max( ar.height, dr.height ) - Math.min( ar.height, dr.height ) - 15;
+            let ymax = ymin + 2*Math.max( ar.height, dr.height );
+
+            display.setAttribute("viewBox", xmin + ' ' + ymin + ' ' + xmax + ' ' + ymax);
+        }
+
+        ["off", "def"].forEach((prefix) => 
+        {
+            let side = (prefix[0] == "o") ? attackers : defender;
+            let tt_ct = 1; // troop type count (how many types have been filled)
+
+            for (let i = 0; i < troop_type_names.length; i++) 
+            {
+                if (side[troop_type_names[i] + "Count"] > 0)
+                {
+                    console.log(prefix + "_a" + tt_ct + "_" + troop_type_names[i] + "_" + side.side );
+                    let icon = document.getElementById(prefix + "_a" + tt_ct + "_" + troop_type_names[i] + "_" + side.side );
+                    icon.classList.toggle("t_np", false);
+                    icon.classList.toggle("t", true);
+                    tt_ct++;
+                }
+            }
+        });
+
+        modal.style.display = "block";
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+    }
 }
 
 /**
@@ -1542,12 +1638,30 @@ class Battle {
             }
         }
 
+        // choose terrain types for the battle
+        this.terrain = [];
+        let terrain_possible = region_terrain[this._def.region];
+        let i = 0;
+        while (this.terrain.length < 3)
+        {
+            for (let e = 0; e < terrain_types.length; e++)
+            {
+                if (terrain_possible[terrain_types[e]] > 0 && Math.random() < terrain_possible[terrain_types[e]])
+                {
+                    this.terrain[i] = terrain_types[e];
+                    i++;
+                }
+            }
+        }
+        console.log(this.terrain);
+
         // Set tick counter
         this._ticks = 0;
 
         this._off_mod = Math.random()/2;
         this._def_mod = Math.random()/2 + 0.05;
 
+        GameMap.drawBattleWindow( this );
 
         // Put information about the battle in the game log
         gameLog( 
@@ -1790,6 +1904,7 @@ class Battle {
         }
     }
 
+
 }
 
 class Strike {
@@ -1979,7 +2094,7 @@ class Game{
                 }
         });
 
-        
+
         // Apply fog-of-war
         this._applyFogOfWar();
 
@@ -2289,19 +2404,6 @@ class Game{
     _handlePlayerMoves()
     {
         let move_list = this["_queuedMoves_" + this._currentPlayerTurn];
-
-        // Get the modal
-        var modal = document.getElementById("myModal");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        modal.style.display = "block";
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
         
         for (let i = 0; i < move_list.length; i++)
         {
