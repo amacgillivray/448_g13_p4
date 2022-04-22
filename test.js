b5fb9687b49f6e60f7fc1d92c81f993f65a60cfb
m926n810 <m926n810@ku.edu>
2022-04-22 17:40:15 -0500

    first test

--- test.js
+++ test.js
@@ -1,19 +1,28 @@
-
-
 function test(){
-    console.log('Test 1: ' + gameLog());
+    gameLog("hello world");
+    let result = "FAIL";
+    //get best troop count symbol => 300 outputs batallion
+    if( getBestTroopCountSymbol(300) == "battalion") result = "PASS";
+    let rsmessage = "getBestTroopCountSymbol " + result;
+    gameLog(rsmessage);
+
+    //Iscapital region 
+    result = "FAIL";
+    
+    //GameUI => set region owner (test for of, bf, neutral)
+    //Get units in region 
+    //draw movement arrow  
+    //force: alter force, distribute damage side
+    //Unit: update health alter units
+    //Battle: start 
+    //Game: change turn, handle win, handle playermoves 
 }
 
+
+ 
 //Tests
 //Gamelog
 //get best troop count symbol => 300 outputs batallion
-//Iscapital region 
-//GameUI => set region owner (test for of, bf, neutral)
-//Get units in region 
-//draw movement arrow  
-//force: alter force, distribute damage side
-//Unit: update health alter units
-//Battle: start 
-//Game: change turn, handle win, handle playermoves 
+
 
 
