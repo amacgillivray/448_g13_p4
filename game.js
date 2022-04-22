5536aee7a38892a24b1760b87c56af161a54a0fd
Jarrod <jarrodgro@gmail.com>
2022-04-21 19:30:21 -0500

    Added a notifaction modal

--- game.js
+++ game.js
@@ -1192,6 +1192,23 @@ class GameMap {
             }
         }
     }
+
+    static notification(message){
+        // Get the modal
+        var modal = document.getElementById("notif");
+
+        document.getElementById("notif-item").textContent = message;
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[1];
+
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+    }
 }
 
 /**
@@ -1840,6 +1857,7 @@ class Game{
         GameMap.drawClouds();
         this._applyFogOfWar();
         this._applyReinforcements();
+        GameMap.notification("sup bitches");
     }
 
     getRegionForce(region_letter)
